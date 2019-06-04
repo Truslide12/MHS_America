1ddc81225dda4930daaf832d235787a189e73358
Kage Edwards <kagemykel.edwards@gmail.com>
2019-06-03 21:10:05 -0700

    Add StripeServiceProvider to Illuminate container

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ComposerServiceProvider::class,
+        App\Providers\StripeServiceProvider::class,
 
         Zizaco\Entrust\EntrustServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
