670604bb8aaf1de8521570ff9c187405b57a888d
Kage Edwards <kagemykel.edwards@gmail.com>
2019-06-25 14:39:52 -0700

    Account recovery

---
+++ resources/views/account/recovery/password.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.master')
+@use-slim-footer
+
+@section('incls-head')
+    <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/static-footer.css">
+    <link rel="stylesheet" type="text/css" href="/css/login.css">
+@stop
+
+@section('content-above')
+<div id="wrap">
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-md-4 col-md-push-4 col-sm-10 col-sm-push-1">
+            <p class="text-center margin-b">
+                <a href="/"><img src="/img/logo-2014-mini.png" alt="MHS America"></a>
+            </p>
+            @if(Session::get('error') != '')
+            <div class="alert alert-danger" role="alert">
+                {{ Session::get('error') }}
+            </div>
+            @endif
+            <div class="panel panel-default">
+                <div class="panel-heading">Forgot Username</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" action="{{ URL::route('account-recovery-password-post') }}" method="POST" role="form">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="inputUsername3" class="col-sm-3 control-label">
+                            Username</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" name="username" id="inputUsername3" placeholder="Username" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail" class="col-sm-3 control-label">
+                            Email</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" name="email" id="inputEmail" placeholder="Username" required>
+                        </div>
+                    </div>
+                    <div class="form-group last">
+                        <div class="col-sm-offset-3 col-sm-9">
+							<button type="submit" class="btn btn-success cta btn-sm">Reset my password</button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+                <div class="panel-footer">
+                    <p>Remember your username? <a href="{{ URL::route('account-login') }}">Login</a></p>
+                    <span>Don't have an account? <a href="{{ URL::route('account-register') }}">Register</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('content-below')
+</div>
+@stop
