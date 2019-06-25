d76db6fcdbf1bdce1e962f57f41d2b86c191012e
Kage Edwards <kagemykel.edwards@gmail.com>
2019-06-25 15:12:47 -0700

    Account recovery

--- resources/views/account/login.blade.php
+++ resources/views/account/login.blade.php
@@ -22,6 +22,13 @@
                 {{ Session::get('error') }}
             </div>
             @endif
+            @if($errors->count() > 0)
+            @foreach($errors->all() as $error)
+            <div class="alert alert-danger" role="alert">
+                {{ $error }}
+            </div>
+            @endforeach
+            @endif
             @if(Input::get('success', '') != '')
             <div class="alert alert-success" role="alert">
                 {{ Input::get('success') }}
