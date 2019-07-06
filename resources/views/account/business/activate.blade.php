8176275ef565750ca3d8dd4d154b300e1d957c9d
Kage Edwards <kagemykel.edwards@gmail.com>
2019-07-05 17:41:34 -0700

    Stuff

--- resources/views/account/business/activate.blade.php
+++ resources/views/account/business/activate.blade.php
@@ -52,7 +52,7 @@
 					<label for="state">State</label>
 					<select id="state" name="state" class="form-control">
 					<option value="CA">ca</option>
-						@foreach(State::all() as $state)
+						@foreach(\App\Models\State::all() as $state)
 						<option value="{{ $state->id }}" @if(Input::old('state') == $state->id) selected @endif>{{ $state->title }}</option>
 						@endforeach
 					</select>
