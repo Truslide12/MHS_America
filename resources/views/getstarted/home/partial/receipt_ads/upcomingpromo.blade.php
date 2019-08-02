fc2b226a8debf417a121eab7185c7132acc93f43
Anthony Avila <anthonymavila@gmail.com>
2019-08-01 18:00:16 -0700

    Home Promo

---
+++ resources/views/getstarted/home/partial/receipt_ads/upcomingpromo.blade.php
@@ -0,0 +1,26 @@
+@section('incls-head-early')
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+@stop
+
+
+
+
+
+<div style="font-family: voltaire;font-size: 18px;margin-bottom:5px;color:#4358d1;"><strong>Focus on Accuracy:</strong> <span style="color:#31708f;">It's a Team Effort</div>
+<p><i class="fas fa-hands-helping" style="font-size:20px;float:left;margin:10px;"></i>
+	MHS America strives to provide the most current and accurate information as possible. Please be accurate with your listing and be sure to update your home status to <strong>Pending</strong> and <strong>Sold</strong> when applicable. This helps you by avoiding emails and phone calls on homes that have sold months ago. With this purchase you are now welcome to list this home on our platform for 180 Days.
+	
+	<div style="margin-top:14px;padding-left: 20px;">
+	<strong style="color: #f7a248;">Please Note:</strong> you will be required to verify the home is still activate after 90 days. The re-activation link will be sent to your email.
+	</div>
+</p><br>
+@if(1==2)
+<div style="font-family: voltaire;font-size: 18px;margin-bottom:5px;color:#4358d1;"><strong>Don't Forget:</strong> <span style="color:#31708f;">Your Home is set to Private</div>
+<p><i class="fas fa-eye-slash" style="font-size:20px;float:left;margin:10px;"></i>
+	Your home is set to Pivate and not yet visible to the public. Once you complete your home listing, set your home status from Private to Active. 
+</p><br>
+@endif
+<div style="font-family: voltaire;font-size: 18px;margin-bottom:5px;color:#4358d1;"><strong>Coming Soon:</strong> <span style="color:#31708f;">Professional Services</div>
+<p><i class="fas fa-certificate" style="font-size:20px;float:left;margin:10px;"></i>
+	Are you with a <strong>Dealership</strong> or <strong>Sales Agency</strong>? MHS America is developing new ways to serve these and other professionals. Keep an eye on our <a href="">news</a> page for updates!
+</p><br>
