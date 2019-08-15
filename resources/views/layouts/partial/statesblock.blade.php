6a4eb3e678fcb2bf7c22d0798eaf72368879aa9b
Kage Edwards <kagemykel.edwards@gmail.com>
2019-08-14 21:13:43 -0700

    Move cool box to top

--- resources/views/layouts/partial/statesblock.blade.php
+++ resources/views/layouts/partial/statesblock.blade.php
@@ -167,31 +167,6 @@
 		</div>
 	</div>
 
-
-
-<div class="row clearfix nudge white" style="text-align: center;font-size: 1.5em;padding-bottom: 100px;padding-top: 0px;">
-		<div class="col-md-12" style="text-align: center;">
-			<h3 style="color:#005499;font-family:Lato;margin-bottom: 15px;padding-bottom: 20px;">Built for Everyone</h3>
-			<center><div class="thick-hr" style="width: 80%;margin-bottom: 10px;"></div></center>
-		</div>
-		<div class="col-md-3">
-			<a style="font-family: Voltaire;color:#0d0d73;font-weight: bold;" href="/sell-a-mobile-home">Mobile Home Sellers</a>
-		</div>
-		<div class="col-md-3">
-			<a style="font-family: Voltaire;color:#0d0d73;font-weight: bold;" href="/buy-a-mobile-home">Mobile Home Buyers</a>
-		</div>
-		<div class="col-md-3">
-			<a style="font-family: Voltaire;color:#0d0d73;font-weight: bold;" href="/mobile-home-sales-agent">Mobile Home Sales Agents</a>
-		</div>
-		<div class="col-md-3">
-			<a style="font-family: Voltaire;color:#0d0d73;font-weight: bold;" href="/promote-mobile-home-park">Mobile Home Park Owners</a>
-		</div>
-		<div class="col-md-12" style="text-align: center;">
-			<center><div class="thick-hr" style="width: 80%;margin-top: 10px;"></div></center>
-		</div>
-</div>
-
-
 @if( 1==2 )
 	<div class="row clearfix nudge white" style="text-align: center;font-size: 1.5em;padding-bottom: 1in;">
 		<div class="col-md-12">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -353,7 +353,28 @@
 	<div>
 		@include('layouts.partial.errors')
 	</div>
-
+	<div class="row clearfix nudge white" style="text-align: center;font-size: 1.5em;padding-bottom: 100px;padding-top: 0px;">
+			<div class="col-md-12" style="text-align: center;">
+				<h3 style="color:#005499;font-family:Lato;margin-bottom: 15px;padding-bottom: 20px;">Built for Everyone</h3>
+				<center><div class="thick-hr" style="width: 80%;margin-bottom: 10px;"></div></center>
+			</div>
+			<div class="col-md-3">
+				<a style="font-family: Voltaire;color:#0d0d73;font-weight: bold;" href="/sell-a-mobile-home">Mobile Home Sellers</a>
+			</div>
+			<div class="col-md-3">
+				<a style="font-family: Voltaire;color:#0d0d73;font-weight: bold;" href="/buy-a-mobile-home">Mobile Home Buyers</a>
+			</div>
+			<div class="col-md-3">
+				<a style="font-family: Voltaire;color:#0d0d73;font-weight: bold;" href="/mobile-home-sales-agent">Mobile Home Sales Agents</a>
+			</div>
+			<div class="col-md-3">
+				<a style="font-family: Voltaire;color:#0d0d73;font-weight: bold;" href="/promote-mobile-home-park">Mobile Home Park Owners</a>
+			</div>
+			<div class="col-md-12" style="text-align: center;">
+				<center><div class="thick-hr" style="width: 80%;margin-top: 10px;"></div></center>
+			</div>
+	</div>
+	@if ( $demo == 1 )
 	<div class="row clearfix nudge white">
 		<div class="col-md-12">
 			<div class="page-header no-margin-t">
@@ -363,7 +384,7 @@
 	</div>
 
 
-	@if ( $demo == 1 )
+
 	<div class="row clearfix nudge white" style="padding-bottom: 1in;">
 		<div class="col-md-12">
 			<div class="page-header no-margin-t">
@@ -457,11 +478,7 @@
 			</div>
 		</div>
 	</div>
-	@else
 
-	@endif
-
-	@if ( $demo == 1 )
 		@include('layouts.partial.actionblock')
 
 		@include('layouts.partial.statesblock')
