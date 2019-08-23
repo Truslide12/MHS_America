7f12aa2d0e4fb6cc3733bcb6e315b0f9e1e095a6
Kage Edwards <kagemykel.edwards@gmail.com>
2019-08-22 21:37:54 -0700

    Reports - empty text

--- resources/views/admin/layouts/partial/side-moderation.blade.php
+++ resources/views/admin/layouts/partial/side-moderation.blade.php
@@ -1,5 +1,5 @@
 <li class="@if(Request::is('luna/moderation*')) active @endif">
-            <a href="{{ URL::route('admin-browse-users') }}">
-              <i class="fa fa-flag"></i><span class="link-title"> Moderation</span>
+            <a href="{{ URL::route('admin-moderation') }}">
+              <i class="fa fa-flag"></i><span class="link-title"> Reports</span>
             </a> 
           </li>
