f48153206c6cfd6fbc64bf721b147623e952f062
Anthony Avila <anthonymavila@gmail.com>
2019-08-28 17:05:06 -0700

    wording

--- resources/views/getstarted/home/notloggedin.blade.php
+++ resources/views/getstarted/home/notloggedin.blade.php
@@ -266,7 +266,8 @@
 		    			<ul>
 		    				<li>You will do business under your employers business account.</li>
 		    				<li>You will be required to input an invitation code before joining.</li>
-		    				<li>Your employer can be shown on your profile.</li>
+		    				<li>Your invitation code can be aquired via the admin for this company.</li>
+\		    				<li>Your employer can be shown on your profile.</li>
 		    			</ul>
 		    			<br>
 		    			<strong>If your employer is not on MHS America:</strong>
