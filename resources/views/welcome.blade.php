384f2d2f2394c319830887a607fe46a55208347c
Anthony Avila <anthonymavila@gmail.com>
2019-08-29 21:53:48 -0700

    remove local fa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,6 @@
 @section('incls-head-early')
 	<link rel="stylesheet" type="text/css" href="{{ URL::route('welcome') }}/css/widgets.css">
 	<link rel="stylesheet" type="text/css" href="{{ URL::route('welcome') }}/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="/css/font-awesome/css/font-awesome.min.css">
 
 @stop
 
