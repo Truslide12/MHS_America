c6e14a4f08e205cb3a7dc841b08b368a37fc0bbd
Kage Edwards <kagemykel.edwards@gmail.com>
2019-10-01 23:09:38 -0700

    Comm of the week

--- resources/views/layouts/partial/home-elements/community-of-the-week.blade.php
+++ resources/views/layouts/partial/home-elements/community-of-the-week.blade.php
@@ -1,7 +1,7 @@
 
 <!-- Start Community of the Week section -->
 	<div class="row clearfix nudge white community-of-the-week-section">
-		<div class="page-header no-margin-t visible-xs">
+		<div class="page-header no-margin-y visible-xs">
 			<h1 class="no-margin-t text-center" style="text-transform: capitalize;font-family:Voltaire;">Community of the Week</h1>
 		</div>
 		<div class="comm_week_box">
@@ -11,10 +11,10 @@
 			</div>
 			<div class="comm_week_text">
 				<div class="clearfix">
-				<strong>{{ $community_of_week->title }}</strong>
-				<strong>Community of the Week - {{ $community_of_week->week }}</strong>
+				<strong><a href="/profile/{{ $community_of_week->id }}">{{ $community_of_week->title }}</a></strong>
+				<strong>{{ $community_of_week->week }}</strong>
 				</div>
-				{!! $community_of_week->description !!}
+				{{ $community_of_week->description }}
 			</div>
 		</div>
 	</div>
