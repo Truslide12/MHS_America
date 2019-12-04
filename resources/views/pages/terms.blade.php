2fb157cbab5ba2ca13428a01ec4ac46c31ce3b30
Kage Edwards <kagemykel.edwards@gmail.com>
2019-12-04 13:51:00 -0800

    Terms of Use

--- resources/views/pages/terms.blade.php
+++ resources/views/pages/terms.blade.php
@@ -7,17 +7,62 @@ $expand_title = true;
 @endphp
 
 @section('page-title')
-Terms of Use
+Terms and Conditions of Use <small>was last updated and is effective as of December 4, 2019.</small>
 @stop
 
 @section('page-content')
-<div class="row">
-	<div class="col-md-6 col-md-offset-3">
-		<div class="panel panel-default">
-			<div class="panel-body">
-				<h4 class="text-center text-muted"><em>Page under maintenance</em></h4>
-			</div>
-		</div>
-	</div>
-</div>
+</>
+
+<h4>Permission to Use</h4>
+
+<p>Your permission to use and access this Site is limited and your license to use or access our Site shall terminate if you violate any of these restrictions or may be terminated by us at any time for no reason at all.</p>
+
+<p>By using our Site, you agree not to:</p>
+
+<ul>
+	<li>modify or copy any material or Services;</li>
+	<li>attempt to decompile or reverse engineer any software contained on the Site or that supports the Site or any Services;</li>
+	<li>engage in any data mining, data harvesting, data extracting or any other similar activity in relation to this Site, or while using this Site; or</li>
+	<li>transfer the materials to another person or entity or &ldquo;mirror&rdquo; the material on any other server.</li>
+</ul>
+
+<h4>Posted Content</h4>
+
+<p>In these Terms and Conditions, your &ldquo;Content&rdquo; shall mean any text, images, audio, video, or other material, including advertisements, comments or feedback, you choose to post on or submit to us via this Site. With respect to your Content, by posting it in a way that is visible to the public, you grant us a non-exclusive, worldwide, irrevocable, royalty-free, sublicensable license to use, reproduce, adapt, publish, translate and distribute it on our Site and in any and all media. Your Content must be your own and must not be infringing on any third party's rights or violate any of the restrictions in the subsection below. We reserve the right to remove any of your Content from our Site at any time, and for any reason, without notice.</p>
+
+<h4>Disclaimer</h4>
+
+<p>Advertisements and listings shown may be from a third-party and we do not guarantee their accuracy. Prices, terms, promotions, features, options, floor plans, elevations, designs, specifications, square footages, and descriptions are subject to change without notice. Square footages are approximate and may vary. Any information such as but not limited to community or neighborhood benefits, features, descriptions, open spaces, and school information are not guaranteed, and are subject to change or modification at any time. Be sure to research and inspect the community and mobile home carefully yourself.</p>
+
+<h4>Dedication to Equal Housing Opportunity</h4>
+
+<p>No seller or renter of mobile homes is allowed to discriminate in the sale, rental and financing of property on the basis of race, color, religion, sex, handicap, familial status, or national origin. No listings, advertisements, or postings are permitted to communicate preference or limitations towards these protected categories. Under the law, a home seller or landlord cannot establish discriminatory terms or conditions in the purchase or rental; deny that housing is available, or advertise that the property is available only to persons of a certain race, color, religion, sex, handicap, familial status, or national origin. If you see an advertisement or encounter a seller or landlord which violates these policies, please let us know immediately.</p>
+
+<h4>Limitation of Liability</h4>
+
+<p>IN NO EVENT WILL WE, OUR AFFILIATES OR OUR OR THEIR LICENSORS, SERVICE PROVIDERS, AGENTS, EMPLOYEES, OFFICERS OR DIRECTORS BE LIABLE FOR DAMAGES OF ANY KIND, UNDER ANY LEGAL THEORY, ARISING OUT OF OR IN CONNECTION WITH YOUR USE, OR INABILITY TO USE, OUR SITE, ANY WEBSITES LINKED TO OR FROM IT, ANY CONTENT ON OUR SITE OR ON SUCH OTHER WEBSITES, OR ANY SERVICES OR ITEMS OBTAINED THROUGH OUR SITE OR SUCH OTHER WEBSITES, INCLUDING ANY DIRECT, INDIRECT, SPECIAL, INCIDENTAL, CONSEQUENTIAL OR PUNITIVE DAMAGES, INCLUDING BUT NOT LIMITED TO, PERSONAL INJURY, PAIN AND SUFFERING, EMOTIONAL DISTRESS, LOSS OF REVENUE, LOSS OF PROFITS, LOSS OF BUSINESS OR ANTICIPATED SAVINGS, LOSS OF USE, LOSS OF GOODWILL, LOSS OF DATA, AND WHETHER CAUSED BY TORT (INCLUDING NEGLIGENCE), BREACH OF CONTRACT OR OTHERWISE, EVEN IF FORESEEABLE. THE FOREGOING DOES NOT AFFECT ANY LIABILITY WHICH CANNOT BE EXCLUDED OR LIMITED UNDER APPLICABLE LAW.</p>
+
+<h4>Indemnification</h4>
+
+<p>You agree to defend, indemnify and hold harmless us, our affiliates, licensors and service providers, and our and their respective officers, directors, employees, contractors, agents, licensors, suppliers, successors and assigns from and against any claims, liabilities, damages, judgments, awards, losses, costs, expenses or fees (including reasonable attorneys' fees) arising out of or relating to your violation of these Terms and Conditions and your use of our Site, including, but not limited to, any use of our Site's content, services and products other than as expressly authorized in these Terms and Conditions or your use of any information obtained from our Site.</p>
+
+<h4>Links</h4>
+
+<p>We have not reviewed all of the third-party websites linked to by the Site and are not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by us of the third-party website. Navigation to and use of any such linked website is at the user's own risk.</p>
+
+<h4>Site Terms of Use Modifications</h4>
+
+<p>We may revise these Terms and Conditions for the Site at any time without notice. By accessing or using the Site, you are agreeing to be bound by the then current version of these Terms and Conditions.</p>
+
+<h4>Termination</h4>
+
+<p>We may terminate your access to the Site, without cause or notice, which may result in the forfeiture and destruction of all information associated with you. All provisions of this Agreement that by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity, and limitations of liability.</p>
+
+<h4>Governing Law</h4>
+
+<p>This Agreement (and any further rules, policies, or guidelines incorporated by reference) shall be governed and construed in accordance with the laws of the State of California, United States of America, without giving effect to any principles of conflicts of law.</p>
+
+<h4>Entire Agreement</h4>
+
+<p>These Terms and Conditions, our Privacy Policy and any other document we deem relevant constitute the sole and entire agreement between you and us with respect to our Site and supersede all prior and contemporaneous understandings, agreements, representations and warranties, both written and oral, with respect to our Site</p>
 @stop
